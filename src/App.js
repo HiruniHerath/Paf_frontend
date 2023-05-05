515a2a41158bb11c3d5d5849f786da804788cf34
IT21026034 <IT21026034@my.sliit.lk>
2023-05-06 00:29:50 +0530

    profile updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Register from './pages/register'
 import Offers from './pages/offers'
 import CreatePost from './pages/createpost'
 import ViewPost from './pages/viewpost'
-
+import Createprofile from './pages/createprofile'
+import Editprofile from './pages/editprofile'
+import Profile from './pages/profile'
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
       <Route exact path="/offer" element={<Offers />} />
       <Route exact path="/createpost" element={<CreatePost />} />
       <Route exact path="/viewpost" element={<ViewPost />} />
+      <Route exact path="/createprofile" element={<Createprofile />} />
+      <Route exact path="/editprofile" element={<Editprofile />} />
+      <Route exact path="/profile" element={<Profile />} />
       </Routes>
     </BRouter>
   );
---
+++ src/pages/profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Nav from '../components/nav'
+import { Form, FormControl, InputGroup, Col, Row, Button, Card, Image } from "react-bootstrap";
+
+import { Container } from 'react-bootstrap';
+
+
+export default function Profile() {
+    return (
+
+        <><Nav></Nav><section>
+
+
+
+
+            <div style={{ backgroundImage: `url(${"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUbtpVKA4u4U9aFXDqhxWJdFUD9nMG7gjIGQ&usqp=CAU"})`, width: "100%", objectFit: "cover", height: "200px" }}>
+            </div>
+        </section></>
+
+
+
+
+
+
+    
+
+   
+
+
+    
+);
+};
+
